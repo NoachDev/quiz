e9bf6d4bfdd76e8fde631fd162dc852536f3c712
NoachDev <noachdev@jitjat.org>
2022-10-06 19:05:13 +0000

    remove component page_ask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
-import React from "react"
 import Layout_home from "../layouts/home"
 import Head from 'next/head'
-import Page_ask from "../components/page_ask";
 
 function Home(){
 
     return (
     <div>
-        <Head>
-            <title>Questions</title>
-        </Head>
+			<Head>
+				<title>Questions</title>
+			</Head>
 
-        <Layout_home>
-            {/* <Page_ask/> */}
-        </Layout_home>
+			<Layout_home>
+			</Layout_home>
+
     </div>
-    )
+	)
 }
 
 export default Home
